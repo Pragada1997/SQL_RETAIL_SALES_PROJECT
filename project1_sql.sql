2364711e35b685f9d69b3779e5fdd182e20447d0
Pragada Ramesh <ramesh.pragada111@gmail.com>
2025-11-03 10:00:25 +0530

    Project1_sql.sql

--- project1_sql.sql
+++ project1_sql.sql
@@ -1,4 +1,22 @@
+CREATE DATABASE sql_project_1;
+
+CREATE TABLE retail_sales
+(
+    transactions_id INT PRIMARY KEY,
+    sale_date DATE,	
+    sale_time TIME,
+    customer_id INT,	
+    gender VARCHAR(10),
+    age INT,
+    category VARCHAR(35),
+    quantity INT,
+    price_per_unit FLOAT,	
+    cogs FLOAT,
+    total_sale FLOAT
+);
+
 USE SQL_PROJECT_1;
+
 SELECT * FROM RETAIL_SALES;
 
 ---- Checking if any null values are there in the table?
